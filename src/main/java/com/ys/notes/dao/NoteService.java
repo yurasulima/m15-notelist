59131bfedda24e70ef52879f4ebbfe2af8e4fc3e
yurasulima <yurasulima124@gmail.com>
2024-07-14 20:27:12 +0300

    init

---
+++ src/main/java/com/ys/notes/dao/NoteService.java
@@ -0,0 +1,54 @@
+package com.ys.notes.dao;
+
+import com.ys.notes.dao.model.Note;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class NoteService {
+
+    private final List<Note> notes = new ArrayList<>();
+
+
+    public List<Note> listAll() {
+        return notes;
+    }
+
+    public Note add(Note note){
+        int id = notes.size() + 1;
+        note.setId(id);
+        notes.add(note);
+        return note;
+    }
+
+    public void deleteById(long id) {
+        for (int i = 0; i < notes.size(); i++) {
+            if (notes.get(i).getId() == id) {
+                notes.remove(i);
+                return;
+            }
+        }
+    }
+
+    public void update(Note note) {
+        for (int i = 0; i < notes.size(); i++) {
+            if (notes.get(i).getId() == note.getId()) {
+                notes.set(i, note);
+            }
+        }
+    }
+
+    public Note getById(long id) {
+
+        for (Note note : notes) {
+            if (note.getId() == id) {
+                return note;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/com/ys/notes/dao/model/Note.java
@@ -0,0 +1,10 @@
+package com.ys.notes.dao.model;
+
+import lombok.Data;
+
+@Data
+public class Note {
+    private int id;
+    private String title;
+    private String content;
+}
